bfa0f75c0d927eb45865adb7c8868d54c412b558
harshitgupta73 <officialharshit73071@gmail.com>
2025-06-06 10:41:07 +0530

    updated

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 const express = require("express");
 const authRouter = require("./routes/auth");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const cors = require("cors");
 const adminRouter = require("./routes/admin");
 const productRouter = require("./routes/product");
 const userRouter = require("./routes/user");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
+
 app.use(authRouter)
 app.use(adminRouter)
 app.use(productRouter)
 app.use(userRouter)
 
-mongoose.connect("mongodb://localhost:27017/amazonDb").then(()=>{
+mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log("Connection successfully")
 }).catch((e)=>{
     console.log(e)
 })
 
-app.listen(PORT,'0.0.0.0', ()=>{
+app.listen(PORT, ()=>{
     console.log(`SErver is running at port ${PORT}`)
 })
